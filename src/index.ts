7e51050a8256dd055b3c9f1ca8bbc276ff5b89fe
beanandbean <wangsw.a@gmail.com>
2023-02-05 16:14:56 +0000

    Standard commonjs module behaviour

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,14 @@ export * from "./triangulate";
 export { default } from "./triangulate";
 
 export { Edge, Point, Triangle } from "./utils";
+
+// tsc emits emulated ES-modules by default;
+// this snippet makes it a standard commonjs module
+// for commonjs and native ESM interop.
+// we could do this for every module but
+// for now this is only applied to `index.ts`
+Object.defineProperties(
+  exports.default,
+  Object.getOwnPropertyDescriptors(exports)
+);
+exports = module.exports = exports.default;
